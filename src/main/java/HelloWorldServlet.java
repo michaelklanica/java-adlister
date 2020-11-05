bd61e81e317a94b0200329d570134d511b6e327d
Michael Klanica <michael.david.klanica@gmail.com>
2020-11-05 16:31:25 -0600

    BONUS - added counter

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,12 +6,14 @@ import java.io.IOException;
 
 @WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+    private int count = 0;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String name = request.getParameter("name");
+        count += 1;
         if (name == null) {
-            response.getWriter().println("<h1>Hello, World!</h1>");
+            response.getWriter().printf("<h1>Hello, World! This page has been viewed %s times.</h1>", count);
         } else {
-            response.getWriter().printf("<h1>Hello, %s</h1>", name);
+            response.getWriter().printf("<h1>Hello, %s! This page has been viewed %s times.</h1>", name, count);
         }
     }
 }
